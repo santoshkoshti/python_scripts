f460c0242dfd4689e1268e6259aa5d3e58bbc703
santoshkoshti <santosh.koshti100@gmail.com>
2021-03-30 14:33:13 +0530

    text to speech added

--- text_to_speech.py
+++ text_to_speech.py
@@ -7,11 +7,15 @@ from gtts import gTTS
 import os
 from playsound import playsound
 
-text_val = 'Hi Santosh Koshti, Welcome to Python Scripts'
+print("Hi Dude, say something in words")
+
+text = input()
+
+# text = 'Hi Santosh Koshti, Welcome to Python Scripts'
 
 language = 'en'
 
-obj = gTTS(text=text_val, lang=language, slow=False)
+obj = gTTS(text=text, lang=language, slow=False)
 
 obj.save("santosh.mp3")
 
